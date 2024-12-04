99b7e9ffc24650d5e43b505b08d5aed0b120860b
aubarrio <aubarrios142@gmail.com>
2024-12-04 13:42:11 -0800

    out json edge case solve

--- app.py
+++ app.py
@@ -93,8 +93,6 @@ with gr.Blocks() as demo:
 
         #get previous user message
         student_prompt = history[-1]["content"]
-        #get user resource selection
-        selection = radio
         # TODO : include implementation for radio resource selection
         #build prompt that will return context for learning path
         context_prompt = f'''
@@ -201,148 +199,141 @@ with gr.Blocks() as demo:
             }
         ]
 
-        try:
-            #call response
-            response = client.chat.completions.create(
-                model = "llama3-8b-8192",
-                messages = [
-                    {
-                        "role": "user",
-                        "content": prompt
-                    }
-                ],
-                tools = tools,
-                tool_choice = "auto"
-            )
-        except Exception as e:
-            print(e)
-
-
-        #update tokens used
-        INPUT_TOKENS = INPUT_TOKENS + response.usage.prompt_tokens
-        OUTPUT_TOKENS = OUTPUT_TOKENS + response.usage.completion_tokens       
-
-        if response.choices[0].message.tool_calls:
-            #get the arguments of the content
-            functio_args = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
-            #run arguments to function
-            out_json = extract_learning_info(**functio_args)
-            #save the content of our response message
-            content_filename = f"./gradio-tests/content_{trial_name}.txt"
-
-            with open(content_filename, "w+") as file:
-                file.write("Passed through function tool_calls")
-        else:
-            #get the content of our response
-            content = response.choices[0].message.content
-            
-            #save the content of our response message
-            content_filename = f"./gradio-tests/content_{trial_name}.txt"
-
-            with open(content_filename, "w+") as file:
-                file.write(content)
-
-            # #get starting index of JSON object
-            # start_index = content.find("{")
-            # #get ending index of JSON object
-            # end_index = len(content) - content[::-1].find("}")
-            # #get the entire JSON text
-            # json_text = content[start_index:end_index] 
-
-            #get start index of JSON file
-            start_index = min([i for i in [content.find("{"), content.find("[")] if i >= 0])
-            #get end index of JSON file
-            end_index = len(content) - min([i for i in [content[::-1].find("}"), content[::-1].find("]")] if i >= 0])
-            #get the entire JSON text
-            json_text = content[start_index:end_index]       
-
+        #implement out_json edge case
+        out_json = None
+        #keep track of number of trials
+        trial = 0
+        #run process until we have an out_json or less than trials
+        while out_json is None and trial < 2:
+            print(f"Current Trial: {trial}")
             try:
-                #load text to json
-                json_response = json.loads(json_text)
+                #call response
+                response = client.chat.completions.create(
+                    model = "llama3-8b-8192",
+                    messages = [
+                        {
+                            "role": "user",
+                            "content": prompt
+                        }
+                    ],
+                    tools = tools,
+                    tool_choice = "auto"
+                )
             except Exception as e:
-                try:
-                    #list of brackets in string
-                    brackets = []
-                    #mapping of closed brackets to open
-                    bracket_map = {
-                        "}": "{",
-                        "]": "["
-                    }
-                    #inverse bracket map
-                    inverse_map = {v: k for k, v in bracket_map.items()}
-                    #save the character index
-                    char_index = 0
-                    #loop through every character in format string
-                    for char in json_text:
-                        if char == "}" or char == "]":
-                            if brackets[-1] != bracket_map[char]:
-                                #then we must add new bracket
-                                while brackets[-1] != bracket_map[char]:
-                                    #add new bracket and remove
-                                    json_text = json_text[0:char_index] + inverse_map[brackets[-1]] + json_text[char_index:]
-                                    brackets = brackets[:-1]
-                                    # print(brackets)
-                                brackets = brackets[:-1]
-                            else:
-                                #eliminate brackets
-                                brackets = brackets[:-1]
-                        elif char == "{" or char == "[":
-                            brackets.append(char)
-                        #increment index
-                        char_index += 1
+                print(e)
 
-                    #handle edge case where last missing bracket is at the end
-                    while brackets:
-                        json_text = json_text + inverse_map[brackets[0]]
-                        brackets = brackets[1:]
+
+            #update tokens used
+            INPUT_TOKENS = INPUT_TOKENS + response.usage.prompt_tokens
+            OUTPUT_TOKENS = OUTPUT_TOKENS + response.usage.completion_tokens       
+
+            if response.choices[0].message.tool_calls:
+                #get the arguments of the content
+                functio_args = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+                #run arguments to function
+                out_json = extract_learning_info(**functio_args)
+                print("Success! Succesfully loaded using tool_calls")
+                #save the content of our response message
+                content_filename = f"./gradio-tests/content_{trial_name}.txt"
+
+                with open(content_filename, "w+") as file:
+                    file.write("Passed through function tool_calls")
+            else:
+                #get the content of our response
+                content = response.choices[0].message.content
+                
+                #save the content of our response message
+                content_filename = f"./gradio-tests/content_{trial_name}.txt"
+
+                with open(content_filename, "w+") as file:
+                    file.write(content)
+
+                # #get starting index of JSON object
+                # start_index = content.find("{")
+                # #get ending index of JSON object
+                # end_index = len(content) - content[::-1].find("}")
+                # #get the entire JSON text
+                # json_text = content[start_index:end_index] 
+
+                #get start index of JSON file
+                start_index = min([i for i in [content.find("{"), content.find("[")] if i >= 0])
+                #get end index of JSON file
+                end_index = len(content) - min([i for i in [content[::-1].find("}"), content[::-1].find("]")] if i >= 0])
+                #get the entire JSON text
+                json_text = content[start_index:end_index]       
+
+                try:
                     #load text to json
                     json_response = json.loads(json_text)
                 except Exception as e:
-                    print("Failure! Could not load your file to JSON with error", e)
-                # ---------- Previous implementation ----------
+                    try:
+                        #list of brackets in string
+                        brackets = []
+                        #mapping of closed brackets to open
+                        bracket_map = {
+                            "}": "{",
+                            "]": "["
+                        }
+                        #inverse bracket map
+                        inverse_map = {v: k for k, v in bracket_map.items()}
+                        #save the character index
+                        char_index = 0
+                        #loop through every character in format string
+                        for char in json_text:
+                            if char == "}" or char == "]":
+                                if brackets[-1] != bracket_map[char]:
+                                    #then we must add new bracket
+                                    while brackets[-1] != bracket_map[char]:
+                                        #add new bracket and remove
+                                        json_text = json_text[0:char_index] + inverse_map[brackets[-1]] + json_text[char_index:]
+                                        brackets = brackets[:-1]
+                                        # print(brackets)
+                                    brackets = brackets[:-1]
+                                else:
+                                    #eliminate brackets
+                                    brackets = brackets[:-1]
+                            elif char == "{" or char == "[":
+                                brackets.append(char)
+                            #increment index
+                            char_index += 1
 
-                # if e.args[0].startswith("Expecting ',' delimiter:"):
-                #     try:
-                #         #load text to json
-                #         json_response = json.loads(json_text[0:e.pos-1] + "}" + json_text[e.pos:])
-                #     except Exception as e:
-                #         try:
-                #             if e.pos == len(json_text):
-                #                 json_response = json.loads(json_text + "}")
-                #             else:
-                #                 print("Nothing happened!")
-                #         except Exception as e:
-                #             print("Failure!! Could not load your file to JSON with error", e)
-                # else:
-                #     print("Failure!!! Could not load your file to JSON with error", e)
-            try:
-                #get the current keys of json file
-                curr_keys = json_response.keys()
-                if "tool_calls" in curr_keys:
-                    if "parameters" in json_response["tool_calls"][0].keys():
-                        out_json = extract_learning_info(**json_response["tool_calls"][0]["parameters"])
-                    elif "function" in json_response["tool_calls"][0].keys():
-                        out_json = extract_learning_info(**json_response["tool_calls"][0]["function"]["parameters"])
-                    print(f"Success! Succesfully load JSON using tool_calls")
-                elif "parameters" in curr_keys:
-                    out_json = extract_learning_info(**json_response["parameters"])
-                    print(f"Success! Succesfully load JSON using parameters")
-                elif "hard_query" in curr_keys:
-                    out_json = extract_learning_info(**json_response)
-                    print(f"Success! Succesfully load JSON using arguments")
-                elif "properties" in curr_keys:
-                    out_json = extract_learning_info(**json_response["properties"])
-                    print(f"Success! Succesfully load JSON using properties")
-                else:
-                    for key in curr_keys:
-                        if "hard_query" in json_response[key].keys():
-                            print(f"Success! Succesfully load JSON using search")
-                            out_json = extract_learning_info(**json_response[key])
-                        else:
-                            out_json = None
-                            print(f"Failure! Could not process JSON keys")
-            except Exception as e:
-                print(f"Failure! Could not process JSON keys with error", e)
+                        #handle edge case where last missing bracket is at the end
+                        while brackets:
+                            json_text = json_text + inverse_map[brackets[0]]
+                            brackets = brackets[1:]
+                        #load text to json
+                        json_response = json.loads(json_text)
+                    except Exception as e:
+                        print("Failure! Could not load your file to JSON with error", e)
+                try:
+                    #get the current keys of json file
+                    curr_keys = json_response.keys()
+                    if "tool_calls" in curr_keys:
+                        if "parameters" in json_response["tool_calls"][0].keys():
+                            out_json = extract_learning_info(**json_response["tool_calls"][0]["parameters"])
+                        elif "function" in json_response["tool_calls"][0].keys():
+                            out_json = extract_learning_info(**json_response["tool_calls"][0]["function"]["parameters"])
+                        print(f"Success! Succesfully load JSON using tool_calls")
+                    elif "parameters" in curr_keys:
+                        out_json = extract_learning_info(**json_response["parameters"])
+                        print(f"Success! Succesfully load JSON using parameters")
+                    elif "hard_query" in curr_keys:
+                        out_json = extract_learning_info(**json_response)
+                        print(f"Success! Succesfully load JSON using arguments")
+                    elif "properties" in curr_keys:
+                        out_json = extract_learning_info(**json_response["properties"])
+                        print(f"Success! Succesfully load JSON using properties")
+                    else:
+                        for key in curr_keys:
+                            if "hard_query" in json_response[key].keys():
+                                print(f"Success! Succesfully load JSON using search")
+                                out_json = extract_learning_info(**json_response[key])
+                            else:
+                                out_json = None
+                                print(f"Failure! Could not process JSON keys")
+                except Exception as e:
+                    print(f"Failure! Could not process JSON keys with error", e)
+            trial += 1
 
         if out_json:
             json_filename = f"./gradio-tests/queries_{trial_name}.json"
@@ -350,38 +341,40 @@ with gr.Blocks() as demo:
             with open(json_filename, "w+") as file:
                 json.dump(out_json, file)
 
-        #TODO: Summarize entire process
-        summary_text = "This is an example summary text"
-        #build out string to display to chatbot
-        resource_message = f"{summary_text}\n"
-        #gather resources from json
-        for key in out_json:
-            #build level text
-            resource_message = resource_message + f"\n{key.capitalize()} Level\n\n"
-            #build description text
-            resource_message = resource_message + f"Goal: {out_json[key]['description']}\n"
-            #build query text
-            # resource_message = resource_message + f"\tResources:\n"
-            if radio == "Videos":
-                #get search results
-                search_results = json.loads(YoutubeSearch(out_json[key]["query"], max_results=3).to_json())
-                #go through the results
-                index = 1
-                for result in search_results["videos"]:
-                    resource_message = resource_message + f"{index}. {result['title']} : https://www.youtube.com{result['url_suffix']}\n"
-            else:
-                #get search results
-                search_results = search(out_json[key]["query"], advanced=True, num_results=5)
-                #go through the results
-                index = 1
-                for result in search_results:
-                    resource_message = resource_message + f"{index}. {result.title} : {result.url}\n"
-                    index += 1
+            #TODO: Summarize entire process
+            summary_text = "This is an example summary text"
+            #build out string to display to chatbot
+            resource_message = f"{summary_text}\n"
+            #gather resources from json
+            for key in out_json:
+                #build level text
+                resource_message = resource_message + f"\n{key.capitalize()} Level\n\n"
+                #build description text
+                resource_message = resource_message + f"Goal: {out_json[key]['description']}\n"
+                #build query text
+                # resource_message = resource_message + f"\tResources:\n"
+                if radio == "Videos":
+                    #get search results
+                    search_results = json.loads(YoutubeSearch(out_json[key]["query"], max_results=3).to_json())
+                    #go through the results
+                    index = 1
+                    for result in search_results["videos"]:
+                        resource_message = resource_message + f"{index}. {result['title']} : https://www.youtube.com{result['url_suffix']}\n"
+                else:
+                    #get search results
+                    search_results = search(out_json[key]["query"], advanced=True, num_results=5)
+                    #go through the results
+                    index = 1
+                    for result in search_results:
+                        resource_message = resource_message + f"{index}. {result.title} : {result.url}\n"
+                        index += 1
+        else:
+            resource_message = "Could not process your request please try again!\n\n"
         
         #get the total request price
         price = get_request_price(INPUT_TOKENS, OUTPUT_TOKENS)
         #append query price to chatbot
-        price_message = f"Process Completed Succesfully! Total Estimated Price: ${price}"
+        price_message = f"Process Completed! Total Estimated Price: ${price}"
         #add price message to final message
         resource_message = resource_message + f"\n\n{price_message}"
 
