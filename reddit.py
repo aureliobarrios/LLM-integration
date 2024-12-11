3f75b4134759b1765df8e22427f3438696f58647
aubarrio <aubarrios142@gmail.com>
2024-12-11 09:56:56 -0800

    reddit changes

--- reddit.py
+++ reddit.py
@@ -2,24 +2,6 @@ import re
 import json
 import requests
 
-#receive url
-url = input("Insert URL here: ")
-
-#manipulate url for json format
-if url[-1] == "/":
-    #add json formatting
-    url = url[:-1] + '.json'
-else:
-    #add json formatting
-    url = url + '.json'
-
-#make request
-headers = {'User-Agent': 'Mozilla/5.0'}
-page = requests.get(url, headers=headers)
-
-#load in json data
-data = json.loads(page.text)
-
 def regex_links(text):
     #find all the links in a text
     return re.findall(r'https?://[^\s)\]]+', text)
@@ -47,14 +29,30 @@ def link_search(comment):
             links = links + link_search(reply)
     return links
 
-#get links mentioned in post
-links = regex_links(data[0]['data']['children'][0]['data']['selftext'])
+def get_links(url):
+    #manipulate url for json format
+    if url[-1] == "/":
+        #add json formatting
+        url = url[:-1] + '.json'
+    else:
+        #add json formatting
+        url = url + '.json'
 
-#get the comments to the original mention
-for comment in data[1]['data']['children']:
-    links = links + link_search(comment)
+    #make request
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    page = requests.get(url, headers=headers)
 
-#remove duplicates
-links = list(set(links))
+    #load in json data
+    data = json.loads(page.text)
 
-print(links)
+    #get links mentioned in post
+    links = regex_links(data[0]['data']['children'][0]['data']['selftext'])
+
+    #get the comments to the original mention
+    for comment in data[1]['data']['children']:
+        links = links + link_search(comment)
+
+    #remove duplicates
+    links = list(set(links))
+
+    return links
