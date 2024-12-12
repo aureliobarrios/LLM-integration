d3e6a5a9f2b5b14f91d6b6875c5c21fed4dd1bc4
aubarrio <aubarrios142@gmail.com>
2024-12-12 14:35:27 -0800

    demo updates

--- demo.py
+++ demo.py
@@ -85,21 +85,319 @@ with gr.Blocks() as demo:
     
     #function to return bot output
     def bot(build_type, difficulty, radio, history):
-        #get the previous message
-        prev_message = history[-1]["content"]
+        #trial name to save test
+        os.environ["TRIAL"] = uuid.uuid4().hex[:5]
+        trial_name = os.environ["TRIAL"]
+        #used to calculate query price
+        INPUT_TOKENS = 0 #prompt tokens
+        OUTPUT_TOKENS = 0 #completion tokens
+        #load environment variables and build client
+        load_dotenv()
+        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+        client = Groq(
+            api_key=GROQ_API_KEY
+        )
+        #get previous user message
+        student_prompt = history[-1]["content"]
         #handle input based on different selections
         if build_type == "Learning Path":
             #get the topic
-            topic = prev_message.split(":")[1].lower()
-            #build test message
-            message = f"Prompt Input: I want to learn {topic}"
-            #add message to history
-            history.append({"role": "assistant", "content": message})
-        else:
-            #build message
-            message = f"Prompt Input: {prev_message}"
-            #add streaming functionaility
-            history.append({"role": "assistant", "content": message})
+            topic = student_prompt.split(":")[1].lower()
+            #build student prompt
+            student_prompt = f"I want to learn {topic}"
+        #build prompt that will return context for learning path        
+        context_prompt = f'''
+        {student_prompt}
+
+        Can you build me a learning path to solve this problem that follows these 
+        levels: beginner, intermediate, hard, advanced.
+
+        For each of these levels give me a one sentence query that I can input into
+        my search engine that will return resources that will help me solve my problem.
+
+        Make sure to also include a one sentence description of what the current
+        difficulty level is teaching me.
+        '''
+        #build context for learning path
+        chat_completion = client.chat.completions.create(
+            messages = [
+                {
+                    "role": "user",
+                    "content": context_prompt
+                }
+            ],
+            model = "llama3-8b-8192"
+        )
+        #update tokens used
+        INPUT_TOKENS = INPUT_TOKENS + chat_completion.usage.prompt_tokens
+        OUTPUT_TOKENS = OUTPUT_TOKENS + chat_completion.usage.completion_tokens
+        #get learning path context
+        learning_path_text = chat_completion.choices[0].message.content
+        #save learning path text
+        lp_text_filename = f"./gradio-tests/{trial_name}.txt"
+
+        with open(lp_text_filename, "w+") as file:
+            file.write(learning_path_text)
+
+        #build current prompt
+        prompt = f'''
+        Please extract the following information from the given text and return it as a JSON object:
+
+        beginner_description
+        beginner_query
+        intermediate_description
+        intermediate_query
+        hard_description
+        hard_query
+        advanced_description
+        advanced_query
+
+        This is the body of text to extract the information from:
+        {learning_path_text}
+        '''
+
+        #build tool configuration
+        tools  = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "extract_learning_info",
+                    "description": "Extract information from given text and return as JSON object",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "beginner_description": {
+                                "type": "string",
+                                "description": "Description of what the student is learning in the beginner difficulty level"
+                            },
+                            "beginner_query": {
+                                "type": "string",
+                                "description": "The web query the student will use to gather resources for the beginner difficulty level"
+                            },
+                            "intermediate_description": {
+                                "type": "string",
+                                "description": "Description of what the student is learning in the intermediate difficulty level"
+                            },
+                            "intermediate_query": {
+                                "type": "string",
+                                "description": "The web query the student will use to gather resources for the intermediate difficulty level"
+                            },
+                            "hard_description": {
+                                "type": "string",
+                                "description": "Description of what the student is learning in the hard difficulty level"
+                            },
+                            "hard_query": {
+                                "type": "string",
+                                "description": "The web query the student will use to gather resources for the hard difficulty level"
+                            },
+                            "advanced_description": {
+                                "type": "string",
+                                "description": "Description of what the student is learning in the advanced difficulty level"
+                            },
+                            "advanced_query": {
+                                "type": "string",
+                                "description": "The web query the student will use to gather resources for the advanced difficulty level"
+                            },
+                        },
+                        "required": [
+                            "beginner_description", "beginner_query",
+                            "intermediate_description", "intermediate_query",
+                            "hard_description", "hard_query",
+                            "advanced_description", "advanced_query"
+                        ]
+                    }
+                }
+            }
+        ]
+
+        #implement out_json edge case
+        out_json = None
+        #keep track of number of trials
+        trial = 0
+        #run process until we have an out_json or less than trials
+        while out_json is None and trial < 2:
+            print(f"Current Trial: {trial}")
+            try:
+                #call response
+                response = client.chat.completions.create(
+                    model = "llama3-8b-8192",
+                    messages = [
+                        {
+                            "role": "user",
+                            "content": prompt
+                        }
+                    ],
+                    tools = tools,
+                    tool_choice = "auto"
+                )
+            except Exception as e:
+                print(e)
+
+            #update tokens used
+            INPUT_TOKENS = INPUT_TOKENS + response.usage.prompt_tokens
+            OUTPUT_TOKENS = OUTPUT_TOKENS + response.usage.completion_tokens       
+
+            if response.choices[0].message.tool_calls:
+                #get the arguments of the content
+                functio_args = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+                #run arguments to function
+                out_json = extract_learning_info(**functio_args)
+                print("Success! Succesfully loaded using tool_calls")
+                #save the content of our response message
+                content_filename = f"./gradio-tests/content_{trial_name}.txt"
+
+                with open(content_filename, "w+") as file:
+                    file.write("Passed through function tool_calls")
+            else:
+                #get the content of our response
+                content = response.choices[0].message.content
+                
+                #save the content of our response message
+                content_filename = f"./gradio-tests/content_{trial_name}.txt"
+
+                with open(content_filename, "w+") as file:
+                    file.write(content)
+
+                #get start index of JSON file
+                start_index = min([i for i in [content.find("{"), content.find("[")] if i >= 0])
+                #get end index of JSON file
+                end_index = len(content) - min([i for i in [content[::-1].find("}"), content[::-1].find("]")] if i >= 0])
+                #get the entire JSON text
+                json_text = content[start_index:end_index]       
+
+                try:
+                    #load text to json
+                    json_response = json.loads(json_text)
+                except Exception as e:
+                    try:
+                        #list of brackets in string
+                        brackets = []
+                        #mapping of closed brackets to open
+                        bracket_map = {
+                            "}": "{",
+                            "]": "["
+                        }
+                        #inverse bracket map
+                        inverse_map = {v: k for k, v in bracket_map.items()}
+                        #save the character index
+                        char_index = 0
+                        #loop through every character in format string
+                        for char in json_text:
+                            if char == "}" or char == "]":
+                                if brackets[-1] != bracket_map[char]:
+                                    #then we must add new bracket
+                                    while brackets[-1] != bracket_map[char]:
+                                        #add new bracket and remove
+                                        json_text = json_text[0:char_index] + inverse_map[brackets[-1]] + json_text[char_index:]
+                                        brackets = brackets[:-1]
+                                        # print(brackets)
+                                    brackets = brackets[:-1]
+                                else:
+                                    #eliminate brackets
+                                    brackets = brackets[:-1]
+                            elif char == "{" or char == "[":
+                                brackets.append(char)
+                            #increment index
+                            char_index += 1
+
+                        #handle edge case where last missing bracket is at the end
+                        while brackets:
+                            json_text = json_text + inverse_map[brackets[0]]
+                            brackets = brackets[1:]
+                        #load text to json
+                        json_response = json.loads(json_text)
+                    except Exception as e:
+                        print("Failure! Could not load your file to JSON with error", e)
+                try:
+                    #get the current keys of json file
+                    curr_keys = json_response.keys()
+                    if "tool_calls" in curr_keys:
+                        if "parameters" in json_response["tool_calls"][0].keys():
+                            out_json = extract_learning_info(**json_response["tool_calls"][0]["parameters"])
+                        elif "function" in json_response["tool_calls"][0].keys():
+                            out_json = extract_learning_info(**json_response["tool_calls"][0]["function"]["parameters"])
+                        print(f"Success! Succesfully load JSON using tool_calls")
+                    elif "parameters" in curr_keys:
+                        out_json = extract_learning_info(**json_response["parameters"])
+                        print(f"Success! Succesfully load JSON using parameters")
+                    elif "hard_query" in curr_keys:
+                        out_json = extract_learning_info(**json_response)
+                        print(f"Success! Succesfully load JSON using arguments")
+                    elif "properties" in curr_keys:
+                        out_json = extract_learning_info(**json_response["properties"])
+                        print(f"Success! Succesfully load JSON using properties")
+                    else:
+                        for key in curr_keys:
+                            if "hard_query" in json_response[key].keys():
+                                print(f"Success! Succesfully load JSON using search")
+                                out_json = extract_learning_info(**json_response[key])
+                            else:
+                                out_json = None
+                                print(f"Failure! Could not process JSON keys")
+                except Exception as e:
+                    print(f"Failure! Could not process JSON keys with error", e)
+            trial += 1
+        
+        #handle extracted information
+        if out_json:
+            #build out file name
+            json_filename = f"./gradio-tests/queries_{trial_name}.json"
+            #save extracted information
+            with open(json_filename, "w+") as file:
+                json.dump(out_json, file)
+
+            #handle summary based on build type
+            if build_type == "Learning Path":
+                #get the difficulty chosen
+                selected_difficulty = difficulty.lower()
+                #start building out summary prompt
+                summary_prompt = f'''
+                I would like you to respond like you are an instructor summarizing to a student. Please avoid drawn out responses, keep it concise and to the point.
+                Summarize the topic that the student is learning in 3 sentences. Begin your response with: In your learning path you must learn ...
+
+                Topic: {out_json[selected_difficulty]["description"]}
+                '''
+                #get the index of current level
+                difficulty_index = list(out_json.keys()).index(selected_difficulty)
+                #get the previous levels empty list if beginner level
+                prev_levels = list(out_json.keys())[:difficulty_index]
+                #loop through previous levels
+                if prev_levels:
+                    #add assumptions prompt
+                    summary_prompt = summary_prompt + "\nAlso add a couple of sentences summarizing each of the assumptions mentioned below that the student should already know. Begin this portion of the response with: This learning path assumes you know ...\n"
+                    #loop through previous level
+                    for level in prev_levels:
+                        #add assumption prompt for previous level
+                        summary_prompt = summary_prompt + f"\nAssuming the student has already learned: {out_json[level]["description"]}"    
+            else:
+                #prompt to summarize entire process
+                summary_prompt = f'''
+                I would like you to respond like you are an instructor summarizing to a student. Please avoid drawn out responses, keep it concise and to the point.
+                Summarize what the student is learning in 4 sentences. Begin your response with: In your learning path you must learn ...
+
+                {learning_path_text}
+                '''
+
+            #get summary response from chatbot
+            summary_response = client.chat.completions.create(
+                messages = [
+                    {
+                        "role": "user",
+                        "content": summary_prompt
+                    }
+                ],
+                model = "llama3-8b-8192"
+            )
+            #update tokens used and price 
+            INPUT_TOKENS = INPUT_TOKENS + summary_response.usage.prompt_tokens
+            OUTPUT_TOKENS = OUTPUT_TOKENS + summary_response.usage.completion_tokens
+
+            #get the summary text
+            summary_text = summary_response.choices[0].message.content
+
+
+
         return history
     
     def clear_handle(history):
