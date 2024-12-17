af27d4d4e8bcc1e653bf90b6671dddae023c9c5a
aubarrio <aubarrios142@gmail.com>
2024-12-17 09:49:14 -0800

    database include

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
+import os
 import sqlite3
+from dotenv import load_dotenv
 
-conn = sqlite3.connect("llm-database.db")
+load_dotenv()
+
+DATABASE_NAME = os.environ["DATABASE_NAME"]
+
+conn = sqlite3.connect(DATABASE_NAME)
 
 cursor = conn.cursor()
 
