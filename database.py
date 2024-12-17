1f74a43f3166fd1052daee878dd2480f10e4bd67
aubarrio <aubarrios142@gmail.com>
2024-12-17 10:15:56 -0800

    database test

--- database.py
+++ database.py
@@ -10,3 +10,19 @@ conn = sqlite3.connect(DATABASE_NAME)
 
 cursor = conn.cursor()
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS test (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+)
+''')
+
+# cursor.execute("INSERT INTO test (name) VALUES ('Python')")
+# cursor.execute("INSERT INTO test (name) VALUES ('Javascript')")
+
+conn.commit()
+
+# cursor.execute("SELECT * FROM test WHERE name == 'Python'")
+cursor.execute("SELECT * FROM test")
+for row in cursor.fetchall():
+    print(row)
