c1b20e08dd957f574ab36d858b2168ba314225da
aubarrio <aubarrios142@gmail.com>
2024-12-17 13:43:24 -0800

    postgres include

--- database.py
+++ database.py
@@ -1,28 +1,64 @@
 import os
-import sqlite3
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
+#load environment variables
+DATABASE_HOST = os.environ["DATABASE_HOST"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
+DATABASE_USER = os.environ["DATABASE_USER"]
+DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
+DATABASE_PORT = os.environ["DATABASE_PORT"]
 
-conn = sqlite3.connect(DATABASE_NAME)
+def establish_connection():
+    #establish a connection
+    conn_details = psycopg2.connect(
+        host = DATABASE_HOST,
+        database = DATABASE_NAME,
+        user = DATABASE_USER,
+        password = DATABASE_PASSWORD,
+        port = DATABASE_PORT
+    )
+    #return connection
+    return conn_details
 
-cursor = conn.cursor()
+#function to build tables for database
+def create_tables():
+    #create table query
+    table_query = """
+    CREATE TABLE learning_paths (
+        id SERIAL PRIMARY KEY,
+        trial TEXT,
+        topic TEXT
+    );
+    CREATE TABLE resource_links (
+        id SERIAL PRIMARY KEY,
+        resource TEXT,
+        title TEXT,
+        description TEXT,
+        topic TEXT,
+        difficulty TEXT,
+        validated BOOLEAN,
+        avg_rating REAL,
+        found_time TIMESTAMP
+    );
+    """
+    #get connection
+    conn_details = establish_connection()
+    #build server cursor
+    cursor = conn_details.cursor()
+    #build tables in server
+    cursor.execute(table_query)
+    #print message
+    print("-- Successfully Created Tables --")
+    #commit changes
+    conn_details.commit()
+    #close cursor
+    cursor.close()
+    #close connection
+    conn_details.close()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS test (
-    id INTEGER PRIMARY KEY,
-    name TEXT
-)
-''')
-
-# cursor.execute("INSERT INTO test (name) VALUES ('Python')")
-# cursor.execute("INSERT INTO test (name) VALUES ('Javascript')")
-
-conn.commit()
-
-# cursor.execute("SELECT * FROM test WHERE name == 'Python'")
-cursor.execute("SELECT * FROM test")
-for row in cursor.fetchall():
-    print(row)
+if __name__ == "__main__":
+    #add row to table
+    establish_connection()
