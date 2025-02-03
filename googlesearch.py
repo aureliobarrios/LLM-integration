c6e338648fcfe10a59b64105adbfc32d1ef59078
aubarrio <aubarrios142@gmail.com>
2025-02-03 14:39:24 -0800

    search result class

--- googlesearch.py
+++ googlesearch.py
@@ -29,3 +29,12 @@ def _req(term, results, lang, start, proxies, timeout, safe, ssl_verify, region)
     )
     resp.raise_for_status()
     return resp
+
+class SearchResult:
+    def __init__(self, url, title, description):
+        self.url = url
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
