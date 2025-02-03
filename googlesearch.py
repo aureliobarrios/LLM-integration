e33d1f922cb6b58419a3893bfeb1656f6ff2b93f
aubarrio <aubarrios142@gmail.com>
2025-02-03 14:38:24 -0800

    request function

--- googlesearch.py
+++ googlesearch.py
@@ -3,3 +3,29 @@ from bs4 import BeautifulSoup
 from requests import get
 from urllib.parse import unquote #used to decode the url
 from .user_agents import get_useragent
+
+def _req(term, results, lang, start, proxies, timeout, safe, ssl_verify, region):
+    resp = get(
+        url="https://www.google.com/search",
+        headers={
+            "User-Agent": get_useragent(),
+            "Accept": "*/*"
+        },
+        params={
+            "q": term,
+            "num": results + 2,  # Prevents multiple requests
+            "hl": lang,
+            "start": start,
+            "safe": safe,
+            "gl": region,
+        },
+        proxies=proxies,
+        timeout=timeout,
+        verify=ssl_verify,
+        cookies = {
+            'CONSENT': 'PENDING+987', # Bypasses the consent page
+            'SOCS': 'CAESHAgBEhIaAB',
+        }
+    )
+    resp.raise_for_status()
+    return resp
