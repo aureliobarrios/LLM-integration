7d82aa90e0196402c140ac48c95d3dce0175ccb3
aubarrio <aubarrios142@gmail.com>
2025-03-25 09:35:19 -0700

    changes

--- database-v1.py
+++ database-v1.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #load environment variables
+DATABASE_HOST = os.environ["DATABASE_HOST"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
 DATABASE_USER = os.environ["DATABASE_USER"]
-DATABASE_HOST = os.environ["DATABASE_HOST"]
 DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
 DATABASE_PORT = os.environ["DATABASE_PORT"]
 
@@ -16,15 +16,14 @@ class KnowledgeBase:
     #build constructor
     def __init__(self):
         #establish connection details
+        self.db_host = DATABASE_HOST
         self.db_name = DATABASE_NAME
         self.db_user = DATABASE_USER
-        self.db_host = DATABASE_HOST
         self.db_password = DATABASE_PASSWORD
         self.db_port = DATABASE_PORT
     
     #function to establish connection
     def start_session(self):
-        print(psycopg2)
         #establish a connection
         self.conn_details = psycopg2.connect(
             database = self.db_name,
@@ -56,19 +55,10 @@ class KnowledgeBase:
         try:
             #execute the query
             self.cursor.execute(query)
+
+            print("-- Query Success --")
         except Exception as e:
             print("Connection Error:", e)
-    
-    #function to query database with response output
-    def query_response(self, query):
-        try:
-            #execute the query
-            self.cursor.execute(query)
-            #get the response
-            response = self.cursor.fetchall()
-        except Exception as e:
-            print("Connection Error:", e)
-        return response
     
     #function to build build tables query
     def build_tables(self):
@@ -82,7 +72,6 @@ class KnowledgeBase:
             topic TEXT,
             difficulty TEXT,
             validated BOOLEAN,
-            uploaded BOOLEAN,
             initial_rating INTEGER,
             reddit_rating REAL,
             found_time TIMESTAMP
@@ -90,39 +79,11 @@ class KnowledgeBase:
         """
         #execute query
         self.query(table_query)
-
-    #function that gets validated resources that are not uploaded
-    def get_validated(self):
-        #build the query
-        query = """SELECT * FROM resource_links WHERE validated = 'True' AND uploaded = 'False';"""
-        #execute the query
-        response = self.query_response(query)
-        return response
-    
-    #function that gets resources that have been uploaded but should be removed
-    def get_depreciated(self):
-        #build the query
-        query = """SELECT * FROM resource_links WHERE validated = 'False' AND uploaded = 'True';"""
-        #execute the query
-        response = self.query_response(query)
-        return response
-
-    def update_uploaded(self, url):
-        #build the query
-        query = f"""UPDATE resource_links SET uploaded = 'True' WHERE resource = '{url}';"""
-        #execute the query
-        self.query(query)
-
-    def update_deleted(self, url):
-        #build the query
-        query = f"""UPDATE resource_links SET uploaded = 'False' WHERE resource = '{url}';"""
-        #execute the query
-        self.query(query)
-
+            
     def insert_resource(self, data):
         #build insert query
         insert_query = f"""
-        INSERT INTO resource_links (resource, title, description, topic, difficulty, validated, uploaded, found_time)
+        INSERT INTO resource_links (resource, title, description, topic, difficulty, validated, found_time)
         VALUES (
             '{data['resource']}',
             '{data['title']}',
@@ -130,7 +91,6 @@ class KnowledgeBase:
             '{data['topic']}',
             '{data['difficulty']}',
             '{data['validated']}',
-            '{data['uploaded']}',
             '{data['found_time']}'
         );
         """
@@ -149,12 +109,32 @@ class KnowledgeBase:
         if self.cursor.fetchone()[0] == 0:
             return False
         return True
+
     
 if __name__ == "__main__":
     #build database
     db = KnowledgeBase()
     #start the session
-    db.start_session()   
+    db.start_session()
+
+    
+
+    #loop through different topics
+    # for topic in ["python", "javascript", "sql"]:
+    #     #build current data
+    #     curr_data = {
+    #         "trial": "other",
+    #         "topic": topic
+    #     }
+    #     #build query
+    #     db.insert_learning_path(curr_data)
+
+    # #get rows
+    # rows = db.get_learning_path_by_topic("python")
+
+    # for row in rows:
+    #     print(row)
+        
     #commit changes made in session
     db.commit_session()
     #end session
