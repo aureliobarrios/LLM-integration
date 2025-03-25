6b4b041d0a8a7dad0c13af7c3edb2ac0b50c77a4
aubarrio <aubarrios142@gmail.com>
2025-03-25 09:58:27 -0700

    panel weight include

--- panel.py
+++ panel.py
@@ -1,7 +1,14 @@
 import gradio as gr
 
 with gr.Blocks() as demo:
-    ...
+    #build a row section
+    with gr.Row():
+        #build alpha
+        alpha_slider = gr.Slider(value=0.8, minimum=0.1, maximum=1.9, step=0.1, interactive=True, label="Alpha Weight", info="How much weight is put into Upvote/Downvote proportion")
+        #build beta
+        beta_slider = gr.Slider(value=0.05, minimum=0.01, maximum=0.10, step=0.01, interactive=True, label="Beta Weight", info="How much weight is put into Engagement (Percent of Population)")
+        #build lambda
+        lambda_slider = gr.Slider(value=7, minimum=5, maximum=10, step=1, interactive=True, label="Time Decay", info="How fast a resource will depreciate over time")
 
 if __name__ == "__main__":
     demo.launch(show_error=True)
