5f9338fd44fcf383da4385bcfd5b824dc96af6a1
aubarrio <aubarrios142@gmail.com>
2025-03-25 10:26:05 -0700

    panel

--- panel.py
+++ panel.py
@@ -17,5 +17,14 @@ with gr.Blocks() as demo:
         #build the downvote section
         downvotes = gr.Number(value=10, minimum=0, interactive=True, label="Number of Downvotes")
 
+    #build the engagement section
+    with gr.Row():
+        #build the population section
+        population = gr.Number(value=200, minimum=1, label="Total Population", interactive=True)
+        #build the views section
+        views = gr.Number(value=0, minimum=0, maximum=population.value, label="Views From Population", interactive=True)
+        #build the days section
+        days = gr.Number(value=1, minimum=0, label="Number of Days Since Post", interactive=True)
+
 if __name__ == "__main__":
     demo.launch(show_error=True)
